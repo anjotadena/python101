a657c0f4b47468548465529479d57ac13152807e
Anjo Tadena <tadena.anjo@gmail.com>
2020-04-06 05:41:29 +0800

    Implement chaining operator

---
+++ chaining_comparison.py
@@ -0,0 +1,12 @@
+# age should be between 18 and 25
+
+age = 22
+
+# if age >= 18 and age <65:
+
+# Chaining comparison operator
+if 18 <= age < 65: 
+    print("Eligible")
+
+# MATH Rule
+# 18 <= age < 65
