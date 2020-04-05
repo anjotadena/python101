19361f66ee5888885ddb64238a20782f01a78d67
Anjo Tadena <tadena.anjo@gmail.com>
2020-04-05 21:50:19 +0800

    Basic python variables

---
+++ variables.py
@@ -0,0 +1,9 @@
+# Variables
+# Will be stored in memory
+
+# Note: Python is case sensitive
+students_count = 1000  # Integer
+rating = 4.99  # Float
+is_admin = True  # Boolean
+is_publshed = False  # Boolean
+name = "Anjo"  # String
