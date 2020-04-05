f4b4187c0612fc0b37d1b022343b21642eb16766
Anjo Tadena <tadena.anjo@gmail.com>
2020-04-05 21:53:11 +0800

    Add rules for naming variables

--- variables.py
+++ variables.py
@@ -7,3 +7,10 @@ rating = 4.99  # Float
 is_admin = True  # Boolean
 is_publshed = False  # Boolean
 name = "Anjo"  # String
+
+# Avoid dirty codes
+
+# Rules for naming variables
+# Variables must be descriptive and meaningful
+# Lower case naming
+# used underscore to seperate variable word
