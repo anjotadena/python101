9ff80062eafccf334407ddcef63335619ce6edea
Anjo Tadena <tadena.anjo@gmail.com>
2020-04-06 06:00:47 +0800

    Basic while loops

---
+++ while_loops.py
@@ -0,0 +1,13 @@
+# number = 10
+
+# while number > 0:
+#     print(number)
+
+#     number //= 2 # Ogmented assignment operator
+
+command = ""
+
+while command.lower() != "quit":
+    command = input("> ")
+
+    print("ECHO:", command)
