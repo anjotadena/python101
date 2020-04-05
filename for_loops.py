08d0a6542a62d72c029a708469a1501c48c5bb0b
Anjo Tadena <tadena.anjo@gmail.com>
2020-04-06 05:49:13 +0800

    Implement for else loop

--- for_loops.py
+++ for_loops.py
@@ -5,3 +5,15 @@
 for number in range(1, 6): # Repeated 5 times
     # print(f"Number: {number + 1}")
     print("Attempt", number, number * ".")
+
+
+successful = False
+
+for number in range(5):
+    print("Attempt")
+
+    if successful:
+        print("Success...")
+        break # Exit automatically in loop
+else:
+    print("Attempted 5 times and failed")
