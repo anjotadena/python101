bb7eac5c14691833a6e40b0f3a5e1581157c970f
Anjo Tadena <tadena.anjo@gmail.com>
2020-04-05 22:20:33 +0800

    Basic number manipulation

---
+++ numbers.py
@@ -0,0 +1,13 @@
+number = 1 # Integer
+number = 1.1 # Float
+number = 1 + 2j # Complex number a + bi
+
+print(number)
+
+print(10 + 3) # Addition
+print(10 - 3) # Subsctraction
+print(10 * 3) # Multiplication
+print(10 / 3) # Division
+print(10 // 3) # Square root
+print(10 % 3) # Modulos
+print(10 ** 3) # Power
