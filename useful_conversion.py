771b30e36bb21a87311d40de12f7fd929383319f
Anjo Tadena <tadena.anjo@gmail.com>
2020-04-05 22:30:06 +0800

    Useful conversion/casting variable

---
+++ useful_conversion.py
@@ -0,0 +1,16 @@
+x = input("x: ")
+y = int(x) + 1
+
+print(f"X typeof {type(x)}")
+print(f"Result: {y}")
+
+# Cast the value of x
+# int(x)
+# float(x)
+# bool(x)
+# str(x)
+
+print(f"float: {float(x)}")
+print(f"int: {int(x)}")
+print(f"bool: {bool(x)}")
+print(f"str: {str(x)}")
