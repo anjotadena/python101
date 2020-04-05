a285300effb7f88f4a794ef24092dad2454a2418
Anjo Tadena <tadena.anjo@gmail.com>
2020-04-06 05:30:43 +0800

    Basic control flow

---
+++ control_structures.py
@@ -0,0 +1,10 @@
+temperature = 39
+
+# As long as indented there belong to if block
+if temperature > 38:
+    print("Positive for COVID-19.")
+elif temperature > 33:
+    print("You're good.")
+else:
+    print('Naah...')
+# print("Please take a rest.")
