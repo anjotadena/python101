d22bc32d00bbea6b3bef77de90f86cb762b81401
Anjo Tadena <tadena.anjo@gmail.com>
2020-04-06 05:46:47 +0800

    Basic for loop

---
+++ for_loops.py
@@ -0,0 +1,7 @@
+# Loop to create a repetion
+
+# range start with 0 by default
+# range(start, end, steps)
+for number in range(1, 6): # Repeated 5 times
+    # print(f"Number: {number + 1}")
+    print("Attempt", number, number * ".")
