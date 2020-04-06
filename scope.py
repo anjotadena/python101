c3387188dd422c2352b9879471c6316aa8b96dc0
Anjo Tadena <tadena.anjo@gmail.com>
2020-04-07 06:39:00 +0800

    Define global variable and access it in function

--- scope.py
+++ scope.py
@@ -1,7 +1,10 @@
 # Stays on memory for a longer time
 message = "a" # Global variable
+global_var = 'h'
 
 def greet(message): # Function scope variable
+    global global_var # Access global message variable
+    
     message = 'aa'
 
     print(message)
