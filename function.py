75339d815ca314b650bda30f15d59117c309942b
Anjo Tadena <tadena.anjo@gmail.com>
2020-04-07 05:48:08 +0800

    Define complicated function ever mader

--- function.py
+++ function.py
@@ -4,3 +4,13 @@ def greet(first_name, last_name):
 
 greet("Anjo", "Tadena")
 
+# 1 - Perform a task
+# 2 - Return a value
+
+# round(1.9)
+
+def get_greeting(name):
+    return f"Hi {name}"
+
+
+print(get_greeting("Anjo"))
