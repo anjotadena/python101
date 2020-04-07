e3370ea5e5be6f46deb947913e0408424e382234
Anjo Tadena <tadena.anjo@gmail.com>
2020-04-08 06:19:20 +0800

    Basic unpacking list

--- lists.py
+++ lists.py
@@ -23,3 +23,14 @@ print(letters[0:3]) # Slice
 print(letters[:]) # copy of original list
 print(numbers[::2]) # Steps 
 print(numbers[::-1]) # Reverse a list
+
+
+numbers = [1, 2, 3]
+first = numbers[0]
+second = numbers[1]
+third = numbers[3]
+
+# Unpacking lists
+first, second, third = numbers # Must be equal size
+first, *other_number = numbers # Packing all other in separate list
+first, *other, last = numbers # Unpacking first and last item
