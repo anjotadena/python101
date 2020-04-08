e8bc785ebc0b92f67aa49e7eae15ca6a8a6842e9
Anjo Tadena <tadena.anjo@gmail.com>
2020-04-09 06:21:04 +0800

    Implement basic exception handling

--- exception.py
+++ exception.py
@@ -6,3 +6,13 @@ except ValueError as ex:
     print(ex)
 else:
     print("No exceptions were thrown.")
+finally:
+    print("Cleaning up.")
+
+
+# try:
+    
+# except (ValueError, ZeroDivisionError):
+#     print("Y")
+# finally:
+#     pass
