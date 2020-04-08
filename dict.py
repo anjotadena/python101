740d5ec08b3ab7fa4355e60d12b8425e10768dbe
Anjo Tadena <tadena.anjo@gmail.com>
2020-04-08 17:19:43 +0800

    Dict comprehensions

--- dict.py
+++ dict.py
@@ -31,3 +31,17 @@ for key in point:
 
 for key, value in point.items():
     print(key, value)
+
+
+# Dict comprehensions
+values = []
+
+for x in range(5):
+    values.append(x * 2)
+print(values)
+
+values = [x * 2 for x in range(5)]
+print(values)
+
+values = {x * 2 for x in range(5)}
+print(values)
