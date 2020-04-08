9eec19140a499522d28317a68d5d2ece53105918
Anjo Tadena <tadena.anjo@gmail.com>
2020-04-08 14:39:00 +0800

    Basic list sorting

--- lists.py
+++ lists.py
@@ -73,3 +73,12 @@ letters = ["a", "b", "c"]
 if "d" in letters:
     print(letters.index("d"))
 
+
+
+numbers = [3, 51, 2, 8, 6]
+
+# numbers.sort(reverse=True)
+
+print(sorted(numbers, reverse=True))
+
+print(numbers)
