7fb498d004090bec73f9d4026e559e63df0f334c
Anjo Tadena <tadena.anjo@gmail.com>
2020-04-09 09:14:05 +0800

    Implement good example for inheritance feature

---
+++ good_inheritance.py
@@ -0,0 +1,36 @@
+class InvalidOperationError(Exception):
+    pass
+
+class Stream:
+    def __init__(self):
+        self.opened = False
+
+    def open(self):
+        if self.opened:
+            raise InvalidOperationError("Cannot do this operation. Stream already opened.")
+        self.opened = True
+
+    def close(self):
+        if not self.opened:
+            raise InvalidOperationError("Cannot do this operation. Stream already closed.")
+        self.opened = False
+
+class FileStream(Stream):
+    def read(self):
+        if not self.opened:
+            raise InvalidOperationError("Cannot do this operation. Stream is not yet open.")
+        print("Read file Stream...")
+
+class NetworkStream(Stream):
+    def read(self):
+        if not self.opened:
+            raise InvalidOperationError("Cannot do this operation. Stream is not yet open.")
+        print("Read network stream...")
+
+stream = FileStream()
+
+# stream.read()
+stream.open()
+
+stream.read()
+
